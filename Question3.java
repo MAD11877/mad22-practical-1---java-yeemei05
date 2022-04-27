324df0572ef42b17d516a496972d53e5c41d65fa
yeemei05 <103989954+yeemei05@users.noreply.github.com>
2022-04-27 15:42:39 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,21 @@ public class Question3
 {
   public static void main(String[] args)
   {
+    Scanner myObj = new Scanner(System.in);
+
+    System.out.println("Enter integer:");
+
+    // Numerical input
+    int num = myObj.nextInt();
+    // Output input by user
+    System.out.println("results: " + num*num); 
+
     /**
      * Reads integer entered by the user, multiplies the integer by itself and print out the result.
      * 
      * Hint: in.nextInt() to read integer
      */
      
-    Scanner in = new Scanner(System.in);
     
   }
 }
